bae6bb46b68baf1916a5f9589ba2356d5790f1a5
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-01-26 16:58:49 +0100

    Commenting the code

--- homework2/advancedExample/controllers/cars.js
+++ homework2/advancedExample/controllers/cars.js
@@ -1,5 +1,6 @@
 const Car = require('../models/cars');
 
+// Displaying cars/index view with all cars document from database
 exports.getCars = (req, res) => {
     return Car.find()
         .then(cars => {
@@ -13,19 +14,24 @@ exports.getCars = (req, res) => {
     })
 }
 
+// Just displaying Create Car view
 exports.getCreateCar = (req, res) => {
     return res.render('cars/createCar', { title: 'Create Car' });
 }
 
+// POST controller
 exports.createCar = (req, res) => {
+    // getting model color and year from req.body and sending them to database to create new Car
     const { model, color, year } = req.body;
 
+    // creating object that need to be saved in Car database
     const car = new Car({
         model,
         color,
         year
     })
 
+    // saving it and the end and redirecting the user
     return car.save()
         .then((result) => {
             console.log('Car is created');
